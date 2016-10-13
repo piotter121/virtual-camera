396904c22e3e97cfbbaf0bf2605c17f99d05fb33
piotr <piotr@pysiekIdeaPad>
2016-10-13 11:51:05 +0200

    Operacje na punktach i macierzach

---
+++ src/wirtualna/kamera/model/Matrix.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wirtualna.kamera.model;
+
+/**
+ *
+ * @author Piotr Pyśk
+ */
+public class Matrix {
+
+    private int rows, columns;
+    private double[][] values;
+
+    public Matrix(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+        values = new double[this.rows][this.columns];
+    }
+
+    public static Matrix translation(double xTrans, double yTrans, double zTrans) {
+        Matrix matrix = new Matrix(4, 4);
+        double[][] values = {
+            {1, 0, 0, xTrans},
+            {0, 1, 0, yTrans},
+            {0, 0, 1, zTrans},
+            {0, 0, 0, 1}
+        };
+        matrix.values = values;
+        return matrix;
+    }
+    
+    public static Matrix translationOX(double xTrans) {
+        return translation(xTrans, 0, 0);
+    }
+    
+    public static Matrix translationOY(double yTrans) {
+        return translation(0, yTrans, 0);
+    }
+    
+    public static Matrix translationOZ(double zTrans) {
+        return translation(0, 0, zTrans);
+    }
+
+    public static Matrix rotationOX(double ox) {
+        Matrix m = new Matrix(4, 4);
+        double[][] data = {
+            {1, 0, 0, 0},
+            {0, Math.cos(ox), -Math.sin(ox), 0},
+            {0, Math.sin(ox), Math.cos(ox), 0},
+            {0, 0, 0, 1}
+        };
+        m.values = data;
+        return m;
+    }
+
+    public static Matrix rotationOY(double oy) {
+        Matrix m = new Matrix(4, 4);
+        double[][] data = {
+            {Math.cos(oy), 0, Math.sin(oy), 0},
+            {0, 1, 0, 0},
+            {-Math.sin(oy), 0, Math.cos(oy), 0},
+            {0, 0, 0, 1}
+        };
+        m.values = data;
+        return m;
+    }
+
+    public static Matrix rotationOZ(double oz) {
+        Matrix m = new Matrix(4, 4);
+        double[][] data = {
+            {Math.cos(oz), -Math.sin(oz), 0, 0},
+            {Math.sin(oz), Math.cos(oz), 0, 0},
+            {0, 0, 1, 0},
+            {0, 0, 0, 1}
+        };
+        m.values = data;
+        return m;
+    }
+
+    public void set(int row, int col, double value) {
+        values[row][col] = value;
+    }
+
+    public double get(int row, int col) {
+        return values[row][col];
+    }
+
+    @Override
+    public String toString() {
+        String rep = new String();
+        for (double[] row : values) {
+            rep += "| ";
+            for (double value : row) {
+                rep += value + " ";
+            }
+            rep += " |\n";
+        }
+        return rep;
+    }
+
+    public Matrix multiply(Matrix b) {
+        Matrix a = this;
+        if (a.columns != b.rows) {
+            throw new IllegalArgumentException(a.columns + " != " + b.rows);
+        }
+        Matrix c = new Matrix(a.rows, b.columns);
+        for (int x = 0; x < c.rows; x++) {
+            for (int y = 0; y < c.columns; y++) {
+                double value = 0;
+                for (int i = 0; i < c.rows; i++) {
+                    value += a.get(x, i) * b.get(i, y);
+                }
+                c.set(x, y, value);
+            }
+        }
+        return c;
+    }
+
+    public Point toPoint3D() {
+        return new Point(get(0, 0), get(1, 0), get(2, 0));
+    }
+
+}
---
+++ src/wirtualna/kamera/model/Point.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wirtualna.kamera.model;
+
+/**
+ *
+ * @author Piotr Pyśk
+ */
+public class Point {
+
+    double x, y, z;
+
+    public Point(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public Matrix toMatrix() {
+        Matrix m = new Matrix(4, 1);
+        m.set(0, 0, x);
+        m.set(1, 0, y);
+        m.set(2, 0, z);
+        m.set(3, 0, 1);
+        return m;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ", " + z + ")";
+    }
+
+    public void add(Point another) {
+        this.x += another.x;
+        this.y += another.y;
+        this.z += another.z;
+    }
+
+    public void substract(Point another) {
+        this.x -= another.x;
+        this.y -= another.y;
+        this.z -= another.z;
+    }
+
+    @Override
+    public Point clone() {
+        return new Point(x, y, z);
+    }
+
+}
